f1468cbc4a3779ee3b3d04c86acb9fe4ac418767
Julcilea <julcilea@gmail.com>
2025-08-23 16:50:18 -0300

    Incluindo novos testes e relatorio de testes

--- test/external/transferExternal.test.js
+++ test/external/transferExternal.test.js
@@ -5,9 +5,6 @@ const { expect } = require('chai');
 // Testes
 describe('Transfer', () => {
     describe('POST /transfers', () => {
-
-        let token = null;
-
         beforeEach(async() => {
             // 1) Capturar o Token
             const respostaLogin = await request('http://localhost:3000')
@@ -35,23 +32,39 @@ describe('Transfer', () => {
             expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado')
         });
 
-        it('Quando informo valores válidos eu tenho sucesso com 201 CREATED', async () => {
+        it('Usando Mocks: Quando informo remetente e destinatario inexistentes recebo 400', async () => {
+            const resposta = await request("http://localhost:3000")
+                .post('/transfers')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    from: "jose",
+                    to: "isabelle",
+                    value: 100
+                });
             
-            // 2) Realizar a Transferência
-            const resposta = await request('http://localhost:3000')
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
+        });
+
+        it('Usando Mocks: Quando informo valores válidos eu tenho sucesso com 201 CREATED', async () => {
+            const resposta = await request("http://localhost:3000")
                 .post('/transfers')
                 .set('Authorization', `Bearer ${token}`)
                 .send({
                     from: "julio",
                     to: "priscila",
                     value: 100
-            });
-            
-            expect(resposta.status).to.equal(201);
-            expect(resposta.body).to.have.property('from', 'julio');
-            expect(resposta.body).to.have.property('to', 'priscila');
-            expect(resposta.body).to.have.property('value', 100);
+                });
 
+                console.log(resposta.body);
+
+            //expect(resposta.status).to.equal(201);
+            
+            // Validação com um Fixture
+            const respostaEsperada = require('../fixture/respostas/quandoInformoValoresValidosEuTenhoSucessoCom201Created.json')
+            delete resposta.body.date;
+            delete respostaEsperada.date; 
+            expect(resposta.body).to.deep.equal(respostaEsperada);
         });
     });
 });
